ebf86dc090c9140f95e18c0b8d6bb1dce4118d8f
Jun <cygoh@umich.edu>
2018-02-21 17:28:00 -0800

    feat(amazonGeotarget): enable amazonGeotarget to be queried with an IP address

--- src/amazonGeotarget.js
+++ src/amazonGeotarget.js
@@ -2,33 +2,46 @@ import amazon from 'geo-amazon';
 import GeolocateService from './geolocate';
 
 class AmazonGeotargetService {
-  static async whereabout(provider = 0) {
+  static async whereabout({ provider = 0, ip } = { provider: 0 }) {
     if (provider > 1) {
       return Promise.reject(new Error('Service is not available'));
     }
     let response = null;
     try {
       if (provider === 0) {
-        response = GeolocateService.geolocateIPAPI();
+        response = GeolocateService.geolocateIPAPI(ip);
       } else {
-        response = GeolocateService.geolocateFreeGeoIp();
+        response = GeolocateService.geolocateFreeGeoIp(ip);
       }
     } catch (err) {
       const nextProvider = provider + 1;
-      AmazonGeotargetService.whereabout(nextProvider);
+      AmazonGeotargetService.whereabout(nextProvider, ip);
     }
 
     return response;
   }
 
   static amazonAffiliateURL(countryCode = 'US') {
-    return amazon.store(countryCode);
+    let amazonURL;
+    try {
+      amazonURL = amazon.store(countryCode);
+    } catch (err) {
+      return 'www.amazon.com';
+    }
+    return amazonURL.includes('amazon') ? amazonURL : 'www.amazon.com';
   }
 
-  static async amazonGeotarget() {
-    const response = await AmazonGeotargetService.whereabout();
-    const countryCode = typeof response === 'object' ? response.country_code : response;
-
+  static async amazonGeotarget(ip) {
+    const response = await AmazonGeotargetService.whereabout({ ip })
+      .catch(() => 'www.amazon.com');
+    let countryCode;
+    if (typeof response === 'object') {
+      if (response.country_code) {
+        countryCode = response.country_code;
+      }
+    } else if (response !== 'Undefined') {
+      countryCode = response;
+    }
     return AmazonGeotargetService.amazonAffiliateURL(countryCode);
   }
 }
